d28d318758c615ff174d70eff3ef37a040bd33f3
rgorskov <rgorskov@gmail.com>
2020-12-20 21:01:02 +0500

    Page component fix

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -5,17 +5,19 @@ export default function ({ initialized, title, renderComponent }) {
     return (
         <div>
             <h1>{title}</h1>
-            {() => {
+            {(() => {
                 if (initialized) {
-                    renderComponent();
+                    return renderComponent();
                 } else {
-                    <div className="h-100 d-flex align-items-center justify-content-center">
-                        <h4 className={s.noDataMessage}>
-                            Для начала работы необходимо загрузить данные
-                        </h4>
-                    </div>;
+                    return (
+                        <div className="h-100 d-flex align-items-center justify-content-center">
+                            <h4 className={s.noDataMessage}>
+                                Для начала работы необходимо загрузить данные
+                            </h4>
+                        </div>
+                    );
                 }
-            }}
+            })()}
         </div>
     );
 }
