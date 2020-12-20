3bf880bfdf24d4775361efbd369900fcf9d91880
rgorskov <rgorskov@gmail.com>
2020-12-20 17:15:44 +0500

    App func -> class

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.scss';
@@ -10,25 +11,33 @@ import Risk from '../Risk/Risk';
 import Yield from '../Yield/Yield';
 import BestChoice from '../BestChoice/BestChoice';
 
-function App() {
-  return (
-    <div className="container-fluid g-0 h-100">
-      <div className="h-100 d-flex">
-        <aside className="nav">
-          <Navigation />
-        </aside>
-        <main className="main flex-grow-1">
-          <Route path="/loaddata" component={LoadData} />
-          <Route path="/datainfo" component={DataInfo} />
-          <Route path="/setupparams" component={SetUpParams} />
-          <Route path="/risk" component={Risk} />
-          <Route path="/yield" component={Yield} />
-          <Route path="/bestchoice" component={BestChoice} />
-          <Redirect exact from="/" to="/loaddata" />
-        </main>
-      </div>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            initialized: false,
+        };
+    }
+    render() {
+        return (
+            <div className="container-fluid g-0 h-100">
+                <div className="h-100 d-flex">
+                    <aside className="nav">
+                        <Navigation disableLinks={!this.state.initialized} />
+                    </aside>
+                    <main className="main flex-grow-1">
+                        <Route path="/loaddata" component={LoadData} />
+                        <Route path="/datainfo" component={DataInfo} />
+                        <Route path="/setupparams" component={SetUpParams} />
+                        <Route path="/risk" component={Risk} />
+                        <Route path="/yield" component={Yield} />
+                        <Route path="/bestchoice" component={BestChoice} />
+                        <Redirect exact from="/" to="/loaddata" />
+                    </main>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
