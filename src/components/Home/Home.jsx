8c9bd42b5ce97df019e406171cfc28fc05c3f33e
rgorskov <rgorskov@gmail.com>
2020-12-22 19:01:35 +0500

    строим интервальный вариационный ряд

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,30 +7,73 @@ import Yield from './Yield/Yield';
 import BestChoice from './BestChoice/BestChoice';
 import Page from './Page/Page';
 
+const _ACCURACY_CONST = 100000;
+
 class Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             initialized: false,
-            sourceData: [],
-            normilizedData: [],
+            sourceData: [], // [{id, name, yieldByDays: [{ Date, Value }]]
+            normilizedData: [], // [{id, name, values:[]}]
+            workData: [], // [{id, name, kEval, kUsing, intervalLen, randomVar:[x,p, entryCount]}]
         };
     }
 
-    _normilizeData(notNormilizeData) {
-        const res = notNormilizeData.map((d) => {
-            const name = d.name,
-                id = d.id;
-            const onlyYields = d.yieldByDays
-                .map((ybd) => ybd.Value)
+    _normilizeData(notNormilizeDataArr) {
+        const res = notNormilizeDataArr.map(({ id, name, yieldByDays }) => {
+            const onlyYields = yieldByDays
+                .map((ybd) => Math.round(ybd.Value * _ACCURACY_CONST))
                 .sort((a, b) => a - b);
             const min = onlyYields[0],
                 max = onlyYields[onlyYields.length - 1];
 
-            const normilized = onlyYields.map((y) => (y - min) / (max - min));
+            const normilized = onlyYields.map((y) => {
+                const div = (y - min) / (max - min);
+                return Math.round(div * _ACCURACY_CONST);
+            });
 
             return { id, name, values: normilized };
         });
+        console.log(res);
+        return res;
+    }
+
+    _createWorkData(normilizedDataArr) {
+        const res = normilizedDataArr.map(({ id, name, values }) => {
+            const kEval = 1 + 3.22 * Math.log10(values.length);
+            const kUsing = Math.ceil(kEval);
+            const intervalLen = _ACCURACY_CONST / kUsing;
+            const commonCount = values.length;
+            let randomVar = [];
+
+            for (
+                let i = 1, currX = 0, nextX = intervalLen;
+                i <= kUsing;
+                i++, currX += intervalLen, nextX += intervalLen
+            ) {
+                let entryCount = (i === kUsing
+                    ? values.filter((v) => v >= currX && v <= nextX)
+                    : values.filter((v) => v >= currX && v < nextX)
+                ).length;
+
+                let _x = (currX + nextX) / 2,
+                    p = entryCount / commonCount;
+                let x = _x / _ACCURACY_CONST,
+                    _p = Math.round(_ACCURACY_CONST * p);
+
+                randomVar = randomVar.concat({
+                    x,
+                    p,
+                    _x,
+                    _p,
+                    entryCount,
+                });
+            }
+
+            return { id, name, kEval, kUsing, intervalLen, randomVar };
+        });
+        console.log(res);
         return res;
     }
 
@@ -40,7 +83,7 @@ class Main extends React.Component {
             initialized: true,
             normilizedData: this._normilizeData(sourceData),
         });
-        console.log(this.state.normilizedData);
+        this._createWorkData(this.state.normilizedData);
     }
 
     render() {
